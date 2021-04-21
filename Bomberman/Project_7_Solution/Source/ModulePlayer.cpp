cb9811089843b87744dbf480dd129d0ea4f08e93
Pol Maresch <polmaresch@gmail.com>
2021-04-21 16:52:17 +0200

    update handout 7

---
+++ Bomberman/Project_7_Solution/Source/ModulePlayer.cpp
@@ -0,0 +1,166 @@
+#include "ModulePlayer.h"
+
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleInput.h"
+#include "ModuleRender.h"
+#include "ModuleParticles.h"
+#include "ModuleAudio.h"
+#include "ModuleCollisions.h"
+
+#include "SDL/include/SDL_scancode.h"
+
+
+ModulePlayer::ModulePlayer()
+{
+	// idle animation - just one sprite
+	idleAnim.PushBack({ 18, 2, 15, 22 });
+
+	// move upwards
+	upAnim.PushBack({ 50, 2, 15, 22 });
+	upAnim.PushBack({ 65, 2, 15, 22 });
+	upAnim.PushBack({ 82, 2, 15, 22 });
+	upAnim.loop = false;
+	upAnim.speed = 0.1f;
+
+	// Move down
+	downAnim.PushBack({ 2, 2, 15, 22 });
+	downAnim.PushBack({ 18, 2, 15, 22 });
+	downAnim.PushBack({ 33, 2, 15, 22 });
+	downAnim.loop = false;
+	downAnim.speed = 0.1f;
+
+	leftAnim.PushBack({ 2, 22, 15, 22 });
+	leftAnim.PushBack({ 18, 22, 15, 22 });
+	leftAnim.PushBack({ 33, 22, 15, 22 });
+	leftAnim.loop = false;
+	leftAnim.speed = 0.1f;
+
+	rightAnim.PushBack({ 2, 22, 15, 22 });
+	rightAnim.PushBack({ 18, 22, 15, 22 });
+	rightAnim.PushBack({ 33, 22, 15, 22 });
+	rightAnim.loop = false;
+	rightAnim.speed = 0.1f;
+}
+
+ModulePlayer::~ModulePlayer()
+{
+
+}
+
+bool ModulePlayer::Start()
+{
+	LOG("Loading player textures");
+
+	bool ret = true;
+
+	texture = App->textures->Load("Assets/bomberman/Bomberman.png");
+	currentAnimation = &idleAnim;
+
+	laserFx = App->audio->LoadFx("Assets/laser.wav");
+	explosionFx = App->audio->LoadFx("Assets/explosion.wav");
+
+	position.x = 150;
+	position.y = 120;
+
+	collider = App->collisions->AddCollider({ position.x, position.y, 32, 16 }, Collider::Type::PLAYER, this);
+
+	return ret;
+}
+
+update_status ModulePlayer::Update()
+{
+	// Moving the player with the camera scroll
+	App->player->position.x += 0;
+
+	if (App->input->keys[SDL_SCANCODE_A] == KEY_STATE::KEY_REPEAT)
+	{
+		position.x -= speed;
+		if (currentAnimation != &rightAnim)
+		{
+			rightAnim.Reset();
+			currentAnimation = &rightAnim;
+		}
+	}
+
+	if (App->input->keys[SDL_SCANCODE_D] == KEY_STATE::KEY_REPEAT)
+	{
+		position.x += speed;
+		if (currentAnimation != &leftAnim)
+		{
+			leftAnim.Reset();
+			currentAnimation = &leftAnim;
+		}
+	}
+
+	if (App->input->keys[SDL_SCANCODE_S] == KEY_STATE::KEY_REPEAT)
+	{
+		position.y += speed;
+		if (currentAnimation != &downAnim)
+		{
+			downAnim.Reset();
+			currentAnimation = &downAnim;
+		}
+	}
+
+	if (App->input->keys[SDL_SCANCODE_W] == KEY_STATE::KEY_REPEAT)
+	{
+		position.y -= speed;
+		if (currentAnimation != &upAnim)
+		{
+			upAnim.Reset();
+			currentAnimation = &upAnim;
+		}
+	}
+
+	if (App->input->keys[SDL_SCANCODE_SPACE] == KEY_STATE::KEY_DOWN)
+	{
+		App->particles->AddParticle(App->particles->laser, position.x + 20, position.y, Collider::Type::PLAYER_SHOT);
+		App->audio->PlayFx(laserFx);
+	}
+
+	// If no up/down movement detected, set the current animation back to idle
+	if (App->input->keys[SDL_SCANCODE_S] == KEY_STATE::KEY_IDLE
+		&& App->input->keys[SDL_SCANCODE_W] == KEY_STATE::KEY_IDLE)
+		currentAnimation = &idleAnim;
+
+	collider->SetPos(position.x, position.y);
+
+	currentAnimation->Update();
+
+	if (destroyed)
+	{
+		destroyedCountdown--;
+		if (destroyedCountdown <= 0)
+			return update_status::UPDATE_STOP;
+	}
+
+	return update_status::UPDATE_CONTINUE;
+}
+
+update_status ModulePlayer::PostUpdate()
+{
+	if (!destroyed)
+	{
+		SDL_Rect rect = currentAnimation->GetCurrentFrame();
+		App->render->Blit(texture, position.x, position.y, &rect);
+	}
+
+	return update_status::UPDATE_CONTINUE;
+}
+
+void ModulePlayer::OnCollision(Collider* c1, Collider* c2)
+{
+	if (c1 == collider && destroyed == false)
+	{
+		App->particles->AddParticle(App->particles->explosion, position.x, position.y, Collider::Type::NONE, 9);
+		App->particles->AddParticle(App->particles->explosion, position.x + 8, position.y + 11, Collider::Type::NONE, 14);
+		App->particles->AddParticle(App->particles->explosion, position.x - 7, position.y + 12, Collider::Type::NONE, 40);
+		App->particles->AddParticle(App->particles->explosion, position.x + 5, position.y - 5, Collider::Type::NONE, 28);
+		App->particles->AddParticle(App->particles->explosion, position.x - 4, position.y - 4, Collider::Type::NONE, 21);
+
+		App->audio->PlayFx(explosionFx);
+
+		destroyed = true;
+	}
+}
