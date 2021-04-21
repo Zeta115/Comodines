1260a3ab68d0dd4098d93ab995476c414781dc70
Roger <66650550+rusroger@users.noreply.github.com>
2021-04-21 19:52:46 +0200

    Update ModuleScene.cpp

--- Project_7_Solution/Project_7_Solution/Source/ModuleScene.cpp
+++ Project_7_Solution/Project_7_Solution/Source/ModuleScene.cpp
@@ -29,21 +29,55 @@ bool ModuleScene::Start()
 
 	// Colliders
 
+	//Limits
 	//Bottom
 	App->collisions->AddCollider({ 0, 242, 3930, 16 }, Collider::Type::WALL);
-
 	//Top
-	App->collisions->AddCollider({ 0, 30, 3930, 16 }, Collider::Type::WALL);
-
+	App->collisions->AddCollider({ 0, 33, 3930, 30 }, Collider::Type::WALL);
 	//Left
-	//App->collisions->AddCollider({ 0, 30, 1375, 16 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 0, 33, 24, 600 }, Collider::Type::WALL);
 	//Right
-	//App->collisions->AddCollider({ 0, 30, 3930, 16 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 232, 33, 25, 600 }, Collider::Type::WALL);
 
+	// Stones
+	/// 1r: donde empieza la piedra a partir de la x - 2n: donde empieza la piedra a partir de la y - 3r: ample de la pedra - 4rt: alçada de la pedra
 
 	//First two columns colliders
-	App->collisions->AddCollider({ 40, 32, 48, 56 }, Collider::Type::WALL);
-	//App->collisions->AddCollider({ 1375, 145, 111, 96 }, Collider::Type::WALL);
+	//Fila 1
+	App->collisions->AddCollider({ 40, 80, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 72, 80, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 104, 80, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 136, 80, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 168, 80, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 200, 80, 16, 10 }, Collider::Type::WALL);
+
+	//Fila 2
+	App->collisions->AddCollider({ 40, 112, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 72, 112, 16, 10 }, Collider::Type::WALL);
+	//App->collisions->AddCollider({ 168, 112, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 200, 112, 16, 10 }, Collider::Type::WALL);
+
+	//Fila 3
+	App->collisions->AddCollider({ 40, 144, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 72, 144, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 168, 144, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 200, 144, 16, 10 }, Collider::Type::WALL);
+
+	//Fila 4
+	App->collisions->AddCollider({ 40, 176, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 72, 176, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 104, 176, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 136, 176, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 168, 176, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 200, 176, 16, 10 }, Collider::Type::WALL);
+
+	//Fila 5
+	App->collisions->AddCollider({ 40, 208, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 72, 208, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 104, 208, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 136, 208, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 168, 208, 16, 10 }, Collider::Type::WALL);
+	App->collisions->AddCollider({ 200, 208, 16, 10 }, Collider::Type::WALL);
 
 	// Enemies
 	App->enemies->AddEnemy(ENEMY_TYPE::REDBIRD, 600, 80);
