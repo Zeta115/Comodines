28b3002cc53c5621177eb50def8c58f945872e4b
Zeta115 <polpallares3@gmail.com>
2021-04-29 15:30:31 +0200

    logo screen implementation

--- L10_Project_Gamepad_and_Fullscreen_handout/Source/SceneIntro.cpp
+++ L10_Project_Gamepad_and_Fullscreen_handout/Source/SceneIntro.cpp
@@ -6,6 +6,7 @@
 #include "ModuleAudio.h"
 #include "ModuleInput.h"
 #include "ModuleFadeToBlack.h"
+#include "ModulePlayer.h"
 
 
 SceneIntro::SceneIntro(bool startEnabled) : Module(startEnabled)
@@ -26,6 +27,7 @@ bool SceneIntro::Start()
 	bool ret = true;
 
 	bgTexture = App->textures->Load("Assets/Screens/title.png");
+	logoTexture = App->textures->Load("Assets/Screens/logo.png");
 	App->audio->PlayMusic("Assets/Audio/music/Title_Theme.ogg", 1.0f);
 
 	App->render->camera.x = 0;
@@ -39,16 +41,27 @@ bool SceneIntro::Start()
 
 UpdateResult SceneIntro::Update()
 {
-	if (App->input->keys[SDL_SCANCODE_RETURN] == KeyState::KEY_DOWN)
+	if (App->input->keys[SDL_SCANCODE_RETURN] == KeyState::KEY_DOWN&&sceneCount==1)
 	{
 		App->fade->FadeToBlack(this, (Module*)App->sceneLevel_1, 90);
 	}
-
+	if (App->player->destroyed == true)sceneCount = 0;
 	return UpdateResult::UPDATE_CONTINUE;
 }
 
 UpdateResult SceneIntro::PostUpdate()
 {
-	App->render->DrawTexture(bgTexture, 0, 15, NULL);
+	if (App->player->win == true) sceneCount = 0;
+	if (sceneCount == 0)
+	{
+		App->render->DrawTexture(logoTexture, 0, 0, NULL);
+		App->player->destroyed = false;
+
+		if (App->input->keys[SDL_SCANCODE_RETURN] == KeyState::KEY_DOWN)sceneCount = 1;
+	}
+
+	if (sceneCount == 1)App->render->DrawTexture(bgTexture, 0, 15, NULL);
+
+
 	return UpdateResult::UPDATE_CONTINUE;
 }
