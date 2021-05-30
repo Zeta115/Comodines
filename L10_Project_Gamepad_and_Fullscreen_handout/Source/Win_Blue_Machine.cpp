d9fbe6d726cf577941f56ab6caf8ad90b5e389b4
MaralGS <algarselma@gmail.com>
2021-05-30 17:14:36 +0200

    Update Win_Blue_Machine.cpp

--- L10_Project_Gamepad_and_Fullscreen_handout/Source/Win_Blue_Machine.cpp
+++ L10_Project_Gamepad_and_Fullscreen_handout/Source/Win_Blue_Machine.cpp
@@ -9,7 +9,7 @@ Win_Blue_Machine::Win_Blue_Machine(int x, int y) : Enemy(x, y)
 	// idle animation - just one sprite
 	idleAnim.PushBack({ 240, 177, 15, 25 });	
 
-	collider = App->collisions->AddCollider({ 0, 0, 15, 25 }, Collider::Type::PASIVEFLOWER, (Module*)App->enemies);
+	collider = App->collisions->AddCollider({ 0, 0, 15, 25 }, Collider::Type::BLUEMACHINE, (Module*)App->enemies);
 }
 
 void Win_Blue_Machine::Update()
